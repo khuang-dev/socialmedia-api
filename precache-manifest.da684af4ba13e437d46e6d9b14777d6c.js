1a4c1f490566071102f8e0e33947dd9b7e36172f
Ken Huang <khuang.dev@gmail.com>
2020-06-09 18:46:31 -0700

    Updates

---
+++ precache-manifest.da684af4ba13e437d46e6d9b14777d6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c44af233fd9f36e0b8fb4913ac4f38",
+    "url": "/socialmedia-api/index.html"
+  },
+  {
+    "revision": "0465d52f270451c15503",
+    "url": "/socialmedia-api/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef92af0a069b75015b64",
+    "url": "/socialmedia-api/static/js/2.c2470aba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/socialmedia-api/static/js/2.c2470aba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0465d52f270451c15503",
+    "url": "/socialmedia-api/static/js/main.b08b4412.chunk.js"
+  },
+  {
+    "revision": "72c5ea68782d33e56770",
+    "url": "/socialmedia-api/static/js/runtime-main.af361564.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/socialmedia-api/static/media/logo.5d5d9eef.svg"
+  }
+]);
